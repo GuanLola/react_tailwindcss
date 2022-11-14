4e371715312988e75257ae749e530081f0a3d73b
Guan Yueming <3079756620@qq.com>
2022-11-14 18:21:24 +0800

    first commit

---
+++ src/views/home.js
@@ -0,0 +1,18 @@
+
+function Home() {
+  return (
+    <div className="bg-white transition duration-500">
+      <div className="px-5vw py-9 lg:py-12">
+        <nav className="text-primary mx-auto flex max-w-8xl items-center justify-between">
+          <div>
+            <a className="text-primary underlined focus:outline-none block whitespace-nowrap text-2xl font-medium transition" href="/">
+              <h1>Kent C. Dodds</h1>
+            </a>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
